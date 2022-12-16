1b62272ae340e9e948b4e3c879e296cd4bfe071b
cvionis <cvionis03@gmail.com>
2022-12-15 23:27:04 -0500

    Edit btclone.sh
    Fix corrupted file

--- btclone.sh
+++ btclone.sh
@@ -1,19 +1,15 @@
 #/usr/bin/sh
 
 # A simple shell script allowing the user to pass the urls 
-<<<<<<< HEAD
 # of several remote repositories as arguments and clone them into the current working directory
 
 if [ "$1" = "" ]
 then
     echo "Usage: Enter the urls to clone into the current directory, each separated by a single space"
-=======
-# of several remote repositories as arguments and clone them all into the current working directory
 
 if [ "$1" = "" ]
 then
     echo Usage: Enter the urls to clone into the current directory, each separated by a single space
->>>>>>> 0d349b4562a5c6dd664954e27940f490213a8765
     exit 1
 fi
 
