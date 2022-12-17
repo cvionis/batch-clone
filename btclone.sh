3dde80793b1a7898ecce74faa2a3752ca43beb45
cvionis <cvionis03@gmail.com>
2022-12-16 23:53:58 -0500

    Edit btclone.sh
    Remove whitespace

--- btclone.sh
+++ btclone.sh
@@ -2,7 +2,6 @@
 
 # A simple shell script allowing the user to pass the urls 
 # of several remote repositories as arguments and clone them into the current working directory
-
 if [ "$1" = "" ]
 then
     echo "Usage: Enter the urls to clone into the current directory, each separated by a single space"
